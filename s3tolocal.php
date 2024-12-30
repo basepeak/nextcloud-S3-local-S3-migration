a9ee334059af7476ad7f9c0c3cf5459f185337d7
timycyip <40321245+timycyip@users.noreply.github.com>
2024-12-30 08:03:26 +0000

    autocreate backup folder

--- s3tolocal.php
+++ s3tolocal.php
@@ -161,6 +161,16 @@ if ($result->rowCount() > 1) {
 }
 
 echo "\ndatabase backup...";
+if (!is_dir($PATH_BACKUP)) {
+  if (mkdir($PATH_BACKUP, 0777, true)) {
+      echo "\nINFO: Directory $PATH_BACKUP created successfully.\n";
+  } else {
+      echo "\nERROR: Failed to create directory $PATH_BACKUP.\n";
+      echo "\nERROR: \$PATH_BACKUP folder does not exist\n"; die;
+  }
+} else {
+  echo "\nINFO: Directory $PATH_BACKUP already exists.\n";
+}
 if (!is_dir($PATH_BACKUP)) { echo "\$PATH_BACKUP folder does not exist\n"; die; }
 
 $process = shell_exec('mysqldump --host='.$CONFIG['dbhost'].
